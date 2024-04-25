d3000b9c9606ed0318e313902098d6cb74bdd486
deepak01-hacker <a9649060356@gmail.com>
2024-04-25 21:03:29 +0530

    cors updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const OpenAI = require("openai");
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
