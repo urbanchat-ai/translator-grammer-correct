0c48fa15530d598fe06e60d073ccd925534539be
deepak01-hacker <a9649060356@gmail.com>
2024-04-25 19:41:45 +0530

    gtr

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 const openai = new OpenAI({
-	apiKey: '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'
+	apiKey: 'sk-proj-BIXeuLsyLh9aKVkjzmdHT3BlbkFJ422fguZxQc26x8R71hlE'
 });
 
 app.use(bodyParser.json());
